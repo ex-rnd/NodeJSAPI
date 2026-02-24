c0a581aa488eb54464fb5efd34879c79aba6a7e5
James Kilonzo <jkilonzo2023@gmail.com>
2026-02-24 15:44:01 +0300

    Downloading a file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
-
-import express from 'express';
+import express from "express";
 
 const app = express();
 
+// app.get("/", (req, res) => {
+//     res.send("Hi");
+// });
+
+// app.get("/", (req, res) => {
+//     res.status(404).send("Not Found!");
+// });
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello World!" });
+// });
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+  res.download("tsconfig.json");
 });
 
 app.listen(3000, () => {
-    console.log(`Express is running on 3000`)
-})
+  console.log(`Express is running on 3000`);
+});
